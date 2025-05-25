c79e2047fb4d81fc9195991465c00b900d2313d1
xeroman.kyn <xeroman.k@gmail.com>
2025-05-25 17:14:25 +0900

    feat: actuator 및 prometheus 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
+
     // Swagger UI
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
 
