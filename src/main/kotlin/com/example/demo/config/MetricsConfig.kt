6cb77e1d40843e4de9da59be4c9c2e1d7d457a0c
xeroman.kyn <xeroman.k@gmail.com>
2025-05-25 18:07:32 +0900

    feat:metric 출력 관련 설정 추가

--- src/main/kotlin/com/example/demo/config/MetricsConfig.kt
+++ src/main/kotlin/com/example/demo/config/MetricsConfig.kt
@@ -1,22 +1,17 @@
 package com.example.demo.config
 
-import io.micrometer.core.instrument.Gauge
-import io.micrometer.core.instrument.MeterRegistry
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class MetricsConfig(
-    private val meterRegistry: MeterRegistry
+//    private val meterRegistry: MeterRegistry
 ) {
     init {
-        // CPU 사용량 메트릭 등록
-        Gauge.builder("system.cpu.usage", Runtime.getRuntime()) { rt ->
-            rt.availableProcessors().toDouble()
-        }.register(meterRegistry)
-
-        // 메모리 사용량 메트릭 등록
-        Gauge.builder("system.memory.used", Runtime.getRuntime()) { rt ->
-            (rt.totalMemory() - rt.freeMemory()).toDouble()
-        }.register(meterRegistry)
+//        Gauge.builder("system.cpu.usage", Runtime.getRuntime()) { rt ->
+//            rt.availableProcessors().toDouble()
+//        }.register(meterRegistry)
+//        Gauge.builder("system.memory.used", Runtime.getRuntime()) { rt ->
+//            (rt.totalMemory() - rt.freeMemory()).toDouble()
+//        }.register(meterRegistry)
     }
 }
